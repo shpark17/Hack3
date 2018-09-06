8589b3c67c39583c45a4e0ecd0ca006073ca0aca
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-06 11:58:31 -0500

    Create cellPlan.c

---
+++ cellPlan.c
@@ -0,0 +1,35 @@
+/**
+ *Author: SungHa Park
+ *Date: 9/06/2018
+ *this program help customers to track their mobile data usage
+ */
+ 
+ #include<stdlib.h>
+ #include<stdio.h>
+ #include<math.h>
+ 
+ int main(int argc, char **argv) {
+   
+   double GbPlan, CurrentDay, GbUsed;
+   
+   GbPlan = atof(argv[1]);
+   CurrentDay = atof(argv[2]);
+   GbUsed = atof(argv[3]);
+   
+   double AverageDailyUsed = GbUsed/CurrentDay;
+   double AverageDailyUse = GbPlan/30;
+   double DaysRemaining = 30 - CurrentDay;
+   double ExpectedData = AverageDailyUsed * DaysRemaining;
+   double PerfectDataRate = (GbPlan-GbUsed)/(DaysRemaining);
+   
+   printf("%f days used, %f days remaining\n Average daily used: %f\n", CurrentDay, DaysRemaining, AverageDailyUsed);
+   
+   If(AverageDailyUsed < AverageDailyUse) {
+     printf("You are using less than your average daily use (%fGb/day)\n Continuing to use this low usage, you will miss %f GB.\n To match your data plan, use about %f GB/day.", AverageDataUsed, ExpectedData, PerfectDataRate);
+   }  
+   else if(AverageDailyUsed>AverageDailyUse) {
+     printf("You are using more than your average daily use (%fGb/day)\n Continuing to use this high usage, you will exceed by %f GB.\n To match your data plan, use about %f GB/day.", AverageDataUsed, ExpectedData, PerfectDataRate);
+   }  
+ return 0;
+ }
+   
