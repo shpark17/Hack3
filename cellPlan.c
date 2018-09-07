8f26a187a2a5bfe1c523260f5b70d8a64993fc41
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-07 10:42:23 -0500

    Update cellPlan.c

--- cellPlan.c
+++ cellPlan.c
@@ -19,7 +19,7 @@
    double AverageDailyUsed = GbUsed/CurrentDay;
    double AverageDailyUse = GbPlan/30;
    double DaysRemaining = 30 - CurrentDay;
-   double ExpectedData = (AverageDailyUse * 30) * (AverageDailyUsed * DaysRemaining);
+   double ExpectedData = abs((AverageDailyUse * 30) - (AverageDailyUsed * DaysRemaining));
    double PerfectDataRate = (GbPlan-GbUsed)/(DaysRemaining);
    
    printf("%f days used, %f days remaining\n Average daily used: %f\n", CurrentDay, DaysRemaining, AverageDailyUsed);
